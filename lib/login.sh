587ebe1aa5842a22109812a48dcdc9efbefa54d6
sgohl <8873017+sgohl@users.noreply.github.com>
2023-12-08 17:58:57 +0100

    Update login.sh

--- lib/login.sh
+++ lib/login.sh
@@ -1,5 +1,6 @@
 ##
 ## this Login function causes Guard to return true
+## and also shared amongst multiple visitors/users in different sessions. 
 ## you should definitely override this function to your desired login method
 ##
 
