91657e8cd4a5e8e5b5a13d993b9cdb53bdeaee8b
sgohl <8873017+sgohl@users.noreply.github.com>
2023-12-08 17:19:52 +0100

    Update session.sh

--- lib/session.sh
+++ lib/session.sh
@@ -68,7 +68,7 @@ Session() {
 	## add response header "set-cookie" for storing session in browser cookie
  	## MUST be the first lines of output/response (no body before)
   	## two additional empty lines are required to terminate HEADERS and start BODY
-	echo "set-cookie: session=${UUID}; SameSite=${COOKIE_SAMESITE:-Strict}"
+        echo "set-cookie: session=${UUID}; Path=/; Expires=\"$(date -d "+${COOKIE_LIFETIME:-30} days" +"%a, %d %b %Y %H:%M:%S GMT")\"; SameSite=${COOKIE_SAMESITE:-Strict}"                                                                                                                                                                                                                                                                                                                                                     
  	echo ""
   	echo ""
 
