5e449c0cc4325b8f2c4634b19c95b0d4adb2ca71
sgohl <8873017+sgohl@users.noreply.github.com>
2024-01-05 18:15:26 +0100

    render.sh supports plugs for Pages

--- lib/render.sh
+++ lib/render.sh
@@ -17,6 +17,9 @@ Render() {
 View() {
 
 	## View is a rendered HTML page and has/yields (possibly multiple) Page(s) and/or Fragment(s)
+	## is the direct descendant/representation of URI
+	## Plugs don't support views directly because of not solvable ambiguity in some conditions
+	## you must yield the desired view from the main app view
 
 	VIEW=${1%.html}
 
@@ -34,76 +37,107 @@ View() {
 Page() {
 
 	## Page is the content rendered within a View
+	## URI represents Page, if no view for URI exists
 
-	if [[ ${1} ]]
+	PAGE=${1%.html}
+
+	if [[ -z ${PAGE} ]]
 	then
-	
-		PAGE=${1}
-	
-	else
-	
-		if [[ -z ${PAGE} ]]
-		then
-		
-			PAGE=${URI}
-		    
-		fi
+
+		## if arg1 not given, use URI as PAGE
+
+		PAGE=${URI}
 	
 	fi
-	
-	PAGE=${PAGE%.html}
 
-	## if $1 is empty and no URI, always Render index
-
-	if [[ -z ${PAGE} ]] && [[ -z ${URI} ]]
+	if [[ -z ${PAGE} ]]
 	then
 
+		## PAGE still empty -> Render default Page index
+
 		Render app/pages/index.html
+		return
 
-	else
+	fi
 
-		## if URI is given but file does not exist
+	## PAGE or URI given, traverse to find correct file
 
-		if [[ ! -f app/pages/${PAGE}.html ]]
+	DIRNAME=$(dirname ${PAGE})
+	BASENAME=$(basename ${PAGE})
+
+	SEARCH="app/plug/${DIRNAME}/pages/${URI} app/plug/$(dirname ${DIRNAME})/pages/${BASENAME} app/plug/$(dirname ${DIRNAME})/pages/$(basename $(dirname $PAGE)) app/plug/${DIRNAME}/pages/${BASENAME} app/plug/${DIRNAME}/pages app/pages/${URI} app/pages"
+
+	## DEBUG
+	#echo SEARCH "$SEARCH"
+
+	for DIR in ${SEARCH}
+	do
+
+		#echo TEST ${DIR}/${BASENAME}.html
+
+		if [[ -f ${DIR}/${BASENAME}.html ]]
+		then
+
+			Render ${DIR}/${BASENAME}.html
+			return
+
+		else	
+
+			if [[ -f ${DIR}/index.html ]]
+			then			
+
+				Render ${DIR}/index.html
+				return
+
+			fi
+		
+		fi
+
+	done
+
+	## no exact file match found
+	## $URI_LAST might be positional argument. check for parent html file
+	## URI=yourcrud/edit/123 resolves to yourcrud/edit.html
+
+	PARENT="app/pages/$(dirname ${URI}).html app/plug/${DIRNAME}/pages/$(dirname ${URI}).html"
+
+	for FILE in ${PARENT}
+	do
+
+		if [[ -f ${FILE} ]]
+		then
+
+			Render ${FILE}
+			return
+
+		fi
+
+	done
+
+	## PATH might be directory, Render subdir-index
+
+	SEARCH="app/pages/${URI} app/plug/${DIRNAME}/pages/${URI}"
+
+	for DIR in ${SEARCH}
+	do
+
+		if [[ -d ${DIR} ]]
 		then
 
 			## URI is directory, Render subdir-index
 
-			if [[ -d app/pages/${URI} ]]
-			then
+			echo found $DIR
 
-				Render app/pages/${URI}/index.html
-				return
-			
-			fi
-
-			## $URI_LAST might be positional argument. check for parent html file
-
-			PARENT="app/pages/$(dirname ${URI}).html"
-
-			if [[ -f ${PARENT} ]]
-			then
-			
-				Render ${PARENT}
-				return
-			
-			else
-			
-				## PARENT also not found. Render 404
-				
-				Render app/pages/404.html
-				return
-			
-			fi
-		
-		else
-
-			Render app/pages/${PAGE}.html
+			Render ${DIR}/index.html
+			return
 
 		fi
 
-	fi
+	done
 
+	## ¯\_(ツ)_/¯ (sur)render
+	echo "404 -- not found"
+	#Page 404
 
 }
 
