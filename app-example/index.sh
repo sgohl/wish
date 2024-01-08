af495a0039e0d9db6e0a4b7aea4ffdaebd62d51c
sgohl <8873017+sgohl@users.noreply.github.com>
2024-01-08 14:24:56 +0100

    Update index.sh

--- app-example/index.sh
+++ app-example/index.sh
@@ -1,26 +1,7 @@
 case ${URI} in
 
-        api/*)
-                URI=${URI#*/}
-
-                ## split URI path into array ${URIPATH[@]}
-                IFS='/'; read -ra URIPATH <<< "$URI"; unset IFS
-                COMMAND=${URIPATH[0]}
-
-                source app/api.sh
-        ;;
-
-        admin*)
-
-                Role admin || Redirect
-                View ${URI}
-
-        ;;
-
-
         *)
-                #Guard login
-
+        
                 View ${URI}
 
         ;;
