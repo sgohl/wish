87be9ba32d69f91fb865a6bbed00c62eb09ac1fa
Silvio Gohl <silvio.gohl@unitedprint.com>
2024-01-08 08:54:03 +0000

    docker-entrypoint.sh

--- wish/docker-entrypoint.sh
+++ wish/docker-entrypoint.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 ## runs at container start
-##
-##
-##
+## after main entrypoint (/www/docker-entrypoint.sh)
+## this file will be placed as /www/app/docker-entrypoint.sh
+
+## Plugs handling
+#-find all bin folders and add to $PATH
+#-tbd
+
+
+## Chain app entrypoint
+source /www/app/docker-entrypoint.sh
