58fc953472c45abc67891f7e4c970c5b929b53a5
sgohl <8873017+sgohl@users.noreply.github.com>
2024-01-08 13:03:21 +0100

    Update render.sh

--- wish/lib/render.sh
+++ wish/lib/render.sh
@@ -89,7 +89,7 @@ Page() {
 	## $URI_LAST might be positional argument. check for parent html file
 	## URI=yourcrud/edit/123 resolves to yourcrud/edit.html
 
-	PARENT="app/pages/$(dirname ${URI}).html app/plug/${DIRNAME}/pages/$(dirname ${URI}).html app/plug/${BASENAME}/pages/$(dirname ${URI}).html"
+	PARENT="app/pages/$(dirname ${URI}).html app/plug/${DIRNAME}/pages/$(dirname ${URI}).html app/plug/${BASENAME}/pages/$(dirname ${URI}).html app/plug/$(dirname ${DIRNAME})/pages/$(basename $(dirname $PAGE)).html"
 
 	for FILE in ${PARENT}
 	do
