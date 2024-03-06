07c943c14405d9e14f19fcf98a91c5d1f243ea9c
sgohl <8873017+sgohl@users.noreply.github.com>
2024-03-06 18:15:57 +0100

    Update boot.sh

--- wish/boot.sh
+++ wish/boot.sh
@@ -1,65 +1,67 @@
-Setenv() {
-    if [[ $# -eq 2 ]]
-    then
-        KEY="${1}"
-        VALUE="${2}"
-    
-        if [[ -z ${!KEY} ]]
-        then
-            export ${KEY}="${VALUE}"
-        fi
-    fi
-}
-
-Include() {
-
-	if [[ -f "${1}" ]]
-	then
-
-		source "${1}"
-
-	elif [[ -d "${1}" ]]
-	then
-
- 		## excludes docker-entrypoint.sh + api.sh
-
-		for LIB in $(find "${1}" -type f -not -path '*/.*' -not -name "docker-entrypoint.sh" -not -name "api.sh" -name "*.sh" 2>/dev/null | sort -n)
-		do
-
-			Include ${LIB}
-
-		done
-  
- 	fi
-	
-}
-
-## DBF == Database Files (folder); flat-file storage for simple non-relational data
-Setenv DBF /www/db
-
-## PATH(folder) where session ids are stored. make sure folder exists. you may use app/lib/_hook-pre.sh for 'mkdir -p ${PATH_SESSION}'
-Setenv PATH_SESSION ${DBF}/session
-
-## Cookie lifetime in days added to the current date used in Session()
-Setenv COOKIE_LIFETIME 30
-
-## see https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Set-Cookie#samesitesamesite-value
-Setenv COOKIE_SAMESITE Strict
-
-## Load APP specific .env
-Include .env
-Include app/.env
-
-## Load APPENV dependent .env (dev, prod, ...)
-Include .env.${APPENV}
-Include app/.env.${APPENV}
-
-## Load LOCAL .env
-Include .env.local
-Include app/.env.local
-
-
-## Load libs
-Include lib
-Include app/lib
-Include app/plug
+#!/bin/bash                                                                                                                                                                                                                                                  
+                                                                                                                                                                                                                                                             
+Setenv() {                                                                                                                                                                                                                                                   
+    if [[ $# -eq 2 ]]                                                                                                                                                                                                                                        
+    then                                                                                                                                                                                                                                                     
+        KEY="${1}"                                                                                                                                                                                                                                           
+        VALUE="${2}"                                                                                                                                                                                                                                         
+                                                                                                                                                                                                                                                             
+        if [[ -z ${!KEY} ]]                                                                                                                                                                                                                                  
+        then                                                                                                                                                                                                                                                 
+            export ${KEY}="${VALUE}"                                                                                                                                                                                                                         
+        fi                                                                                                                                                                                                                                                   
+    fi                                                                                                                                                                                                                                                       
+}                                                                                                                                                                                                                                                            
+                                                                                                                                                                                                                                                             
+Include() {                                                                                                                                                                                                                                                  
+                                                                                                                                                                                                                                                             
+        if [[ -f "${1}" ]]                                                                                                                                                                                                                                   
+        then                                                                                                                                                                                                                                                 
+                                                                                                                                                                                                                                                             
+                source "${1}"                                                                                                                                                                                                                                
+                                                                                                                                                                                                                                                             
+        elif [[ -d "${1}" ]]                                                                                                                                                                                                                                 
+        then                                                                                                                                                                                                                                                 
+                                                                                                                                                                                                                                                             
+                ## excludes docker-entrypoint.sh + api.sh                                                                                                                                                                                                    
+                                                                                                                                                                                                                                                             
+                for LIB in $(find "${1}" -type f -not -path '*/.*' -not -name "docker-entrypoint.sh" -not -name "api.sh" -name "*.sh" 2>/dev/null | sort -n)                                                                                                 
+                do                                                                                                                                                                                                                                           
+                                                                                                                                                                                                                                                             
+                        Include ${LIB}                                                                                                                                                                                                                       
+                                                                                                                                                                                                                                                             
+                done                                                                                                                                                                                                                                         
+                                                                                                                                                                                                                                                             
+        fi                                                                                                                                                                                                                                                   
+                                                                                                                                                                                                                                                             
+}                                                                                                                                                                                                                                                            
+                                                                                                                                                                                                                                                             
+## DBF == Database Files (folder); flat-file storage for simple non-relational data                                                                                                                                                                          
+Setenv DBF /www/db                                                                                                                                                                                                                                           
+                                                                                                                                                                                                                                                             
+## PATH(folder) where session ids are stored. make sure folder exists. you may use app/lib/_hook-pre.sh for 'mkdir -p ${PATH_SESSION}'                                                                                                                       
+Setenv PATH_SESSION ${DBF}/session                                                                                                                                                                                                                           
+                                                                                                                                                                                                                                                             
+## Cookie lifetime in days added to the current date used in Session()                                                                                                                                                                                       
+Setenv COOKIE_LIFETIME 30                                                                                                                                                                                                                                    
+                                                                                                                                                                                                                                                             
+## see https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Set-Cookie#samesitesamesite-value                                                                                                                                                           
+Setenv COOKIE_SAMESITE Strict                                                                                                                                                                                                                                
+                                                                                                                                                                                                                                                             
+## Load APP specific .env                                                                                                                                                                                                                                    
+Include /www/.env                                                                                                                                                                                                                                            
+Include /www/app/.env                                                                                                                                                                                                                                        
+                                                                                                                                                                                                                                                             
+## Load APPENV dependent .env (dev, prod, ...)                                                                                                                                                                                                               
+Include /www/.env.${APPENV}                                                                                                                                                                                                                                  
+Include /www/app/.env.${APPENV}                                                                                                                                                                                                                              
+                                                                                                                                                                                                                                                             
+## Load LOCAL .env                                                                                                                                                                                                                                           
+Include /www/.env.local                                                                                                                                                                                                                                      
+Include /www/app/.env.local                                                                                                                                                                                                                                  
+                                                                                                                                                                                                                                                             
+                                                                                                                                                                                                                                                             
+## Load libs                                                                                                                                                                                                                                                 
+Include /www/lib                                                                                                                                                                                                                                             
+Include /www/app/lib                                                                                                                                                                                                                                         
+Include /www/app/plug                                                                                                                                                                                                                                        
