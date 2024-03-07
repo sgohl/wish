d1b71b3bfb32b930e25919e91bf5fe01662ee926
sgohl <8873017+sgohl@users.noreply.github.com>
2024-03-07 21:04:27 +0100

    Update helper.sh

--- wish/lib/helper.sh
+++ wish/lib/helper.sh
@@ -14,3 +14,42 @@ Expect() {
 	fi
 
 }
+
+
+Mail() {
+
+    ## -f : From
+    ## -t : To
+    ## -s : Subject
+
+    if read -t 0
+    then
+
+        CONTENT=$(cat -)
+
+    fi
+
+    if [[ ${MAIL_FROM} ]]
+    then
+        FROM="${MAIL_FROM}"
+    else
+        FROM="${APPNAME} <root@localhost>"
+    fi
+    
+    while getopts ":ft:s:" opt; do
+        case $opt in
+            f) FROM="${OPTARG}";;
+            t) TO="${OPTARG}";;
+            s) SUBJECT="${OPTARG}";;
+        esac
+    done
+
+    /usr/sbin/ssmtp -t << EOF
+From: ${FROM}
+To: ${TO}
+Subject: ${SUBJECT}
+
+${CONTENT}
+EOF
+
+}
